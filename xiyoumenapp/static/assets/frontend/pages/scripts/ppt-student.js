94035ec095fc59d6abc5914e32aafff392c68a43
ericpctiger <ericpctiger@hotmail.com>
2016-10-30 23:00:43 +0800

    update2 webinar_student.html

--- xiyoumenapp/static/assets/frontend/pages/scripts/ppt-student.js
+++ xiyoumenapp/static/assets/frontend/pages/scripts/ppt-student.js
@@ -1,7 +1,7 @@
 // BEGIN PPT CODE
 
 var pptposition;
-var status_source = new EventSource("/stream?channel=changed.status");
+var ppt_source = new EventSource("/stream?channel=changed.ppt");
 
 
 function addPPT(classid, pptname){
@@ -13,7 +13,7 @@ function addPPT(classid, pptname){
 function getPPTPosition(){
     var myeventtype = "newposition"+classstr;
     console.log(myeventtype);
-    status_source.addEventListener("newposition"+classstr,function(event){
+    ppt_source.addEventListener("newposition"+classstr,function(event){
         var data = JSON.parse(event.data);
         pptposition = data.pptposition;
         $("iframe#ppt-area").contents().find("div#page-container").scrollTop(pptposition);
@@ -24,32 +24,32 @@ function getPPTPosition(){
 // Inintial all chatmessage from endpoint /chatlist/
 $(document).ready(function() {
     "use strict";
-    $.getJSON("/info/", function (data) {
-        "use strict";
-        classstr = data.classstr;
-        classid = data.classid;
-    }).then(function(){
-        "use strict";
-        var ppteventtype = "pptinfo" + classstr;
-        status_source.addEventListener(ppteventtype, function(event){
-            var data = JSON.parse(event.data);
-            pptname = data.pptinfo;
-            if (pptname){
-                $("iframe#ppt-area").removeClass("hidden");
-                $("div#button-area").addClass("hidden");
-                addPPT(classid, pptname);
-                $.when(addPPT(classid, pptname)).done(function(){
-                    getPPTPosition();
-                });
-            }else{
-                $("iframe#ppt-area").addClass("hidden");
-                $("div#button-area").removeClass("hidden");
-            }
-        });
-    });
+
 });
 
-
+$.getJSON("/info/", function (data) {
+    "use strict";
+    classstr = data.classstr;
+    classid = data.classid;
+}).then(function(){
+    "use strict";
+    var ppteventtype = "pptinfo" + classstr;
+    ppt_source.addEventListener(ppteventtype, function(event){
+        var data = JSON.parse(event.data);
+        pptname = data.pptinfo;
+        if (pptname){
+            $("iframe#ppt-area").removeClass("hidden");
+            $("div#button-area").addClass("hidden");
+            addPPT(classid, pptname);
+            $.when(addPPT(classid, pptname)).done(function(){
+                getPPTPosition();
+            });
+        }else{
+            $("iframe#ppt-area").addClass("hidden");
+            $("div#button-area").removeClass("hidden");
+        }
+    });
+});
 
 
 
